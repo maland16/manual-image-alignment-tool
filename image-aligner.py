fff6e887ffb88df8941cfacbca460e8781262e63
Devin Malanaphy <accounts@devinmalanaphy.com>
2021-01-24 14:04:13 -0500

    testing git commits in vscode

--- image-aligner.py
+++ image-aligner.py
@@ -1,13 +1,26 @@
-from PIL import Image, ImageFilter
+import PIL
+from PIL import ImageTk, Image, ImageFilter
 import tkinter as tk
 from tkinter import filedialog
+from tkinter import *
+import time
 
 
 
 def main():
-    file_path = filedialog.askopenfilenames(title='Select images to align')
+    window = tk.Tk()
+    window.title("Manual Image Alignment Tool")
+    window.geometry("800x800")
+    window.configure(background='grey')
 
-    print(file_path)
+
+    image_paths = filedialog.askopenfilenames(title='Select images to align')
+    output_folder = filedialog.askdirectory(title='Select output folder')
+
+    img1 = ImageTk.PhotoImage(PIL.Image.open(image_paths[0]))
+    panel = tk.Label(window, image = img1)
+    panel.pack(side = "top", fill = "both", expand = "yes")
+    window.mainloop()
 
 if __name__ == "__main__":
     main()
